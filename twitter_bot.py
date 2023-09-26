a160a868240b8d57e2cffacfe34cb70d032a8842
jarellgalura <jarellgaluraa@gmail.com>
2023-09-26 10:54:59 +0800

    added jp translation

--- twitter_bot.py
+++ twitter_bot.py
@@ -2,16 +2,24 @@ import discord
 import requests
 import asyncio
 import tempfile
-import mtranslate
 from discord.ext import commands
 from discord.ui import Button, View
 from urllib.parse import urlsplit, urlunsplit, urljoin
 import re
+import time
+# Import the translate function from the mtranslate library
+from mtranslate import translate
 
 # Create a bot instance
 intents = discord.Intents.all()
 intents.members = True
 bot = commands.Bot(command_prefix="hn ", intents=intents)
+
+# Create a variable to store the timestamp when the "Kr/En" button was created
+translation_button_created_time = 0
+
+# Create a variable to store the timestamp when the "Jp/En" button was created
+jp_translation_button_created_time = 0
 
 
 async def fetch_media(url):
@@ -87,7 +95,7 @@ async def hn_tweet_link(ctx, tweet_link):
                 # If it starts with "https://twitter.com/", use it directly as the original_link
                 original_link = urljoin("https://twitter.com/", tweet_link)
 
-             # Remove query parameters (everything after "?") from the original_link
+            # Remove query parameters (everything after "?") from the original_link
             original_link = original_link.split("?")[0]
 
             if media_urls or video_urls:
@@ -110,10 +118,11 @@ async def hn_tweet_link(ctx, tweet_link):
                 if any(char >= '가' and char <= '힣' for char in tweet_caption):
                     button_label = "Kr/En"
                     translation_button = Button(
-                        style=discord.ButtonStyle.danger, label=button_label)
+                        style=discord.ButtonStyle.success, label=button_label)
 
                     # Define a callback function for the Translation button
                     async def translate_callback(interaction):
+                        global translation_button_created_time  # Access the global variable
                         nonlocal translated
                         await interaction.response.defer()
 
@@ -123,7 +132,7 @@ async def hn_tweet_link(ctx, tweet_link):
                             button_label = "Translation"
                         else:
                             # Translate the caption
-                            translated_caption = mtranslate.translate(
+                            translated_caption = translate(
                                 tweet_caption, "en", "auto")
                             new_caption = f"{x_emote_syntax} **@{username}** `{formatted_date}`\n\n{translated_caption}"
                             button_label = "Original"
@@ -133,11 +142,66 @@ async def hn_tweet_link(ctx, tweet_link):
                         translation_button.label = button_label
                         translated = not translated
 
+                        # Update the timestamp when the button was created
+                        translation_button_created_time = time.time()
+
+                        # Check if the time since the button was created exceeds 15 minutes
+                        if time.time() - translation_button_created_time > 900:
+                            translation_button.disabled = True  # Disable the button
+                        else:
+                            translation_button.disabled = False  # Enable the button
+
                     # Add the callback to the Translation button
                     translation_button.callback = translate_callback
 
                     # Add the Translation button to the view
                     view.add_item(translation_button)
+
+                # Create the Japanese translation button conditionally
+                jp_translation_button = None
+                jp_translated = False  # Flag to track if Japanese to English translation is applied
+
+                if any(char >= 'あ' and char <= 'ん' for char in tweet_caption):
+                    button_label = "Jp/En"
+                    jp_translation_button = Button(
+                        style=discord.ButtonStyle.success, label=button_label)
+
+                    # Define a callback function for the Japanese to English Translation button
+                    async def jp_translate_callback(interaction):
+                        global jp_translation_button_created_time  # Access the global variable
+                        nonlocal jp_translated
+                        await interaction.response.defer()
+
+                        if jp_translated:
+                            # Revert to the original caption
+                            new_caption = f"{x_emote_syntax} **@{username}** `{formatted_date}`\n\n{tweet_caption}"
+                            button_label = "Jp/En"
+                        else:
+                            # Translate the Japanese caption to English
+                            jp_to_en_translated_caption = translate(
+                                tweet_caption, "en", "ja")
+                            new_caption = f"{x_emote_syntax} **@{username}** `{formatted_date}`\n\n{jp_to_en_translated_caption}"
+                            button_label = "Original"
+
+                        # Update the message content and button label
+                        await original_caption_message.edit(content=new_caption)
+                        jp_translation_button.label = button_label
+                        jp_translated = not jp_translated
+
+                        # Update the timestamp when the button was created
+                        jp_translation_button_created_time = time.time()
+
+                        # Check if the time since the button was created exceeds 15 minutes
+                        if time.time() - jp_translation_button_created_time > 900:
+                            jp_translation_button.disabled = True  # Disable the button
+                        else:
+                            jp_translation_button.disabled = False  # Enable the button
+
+                    # Add the callback to the Japanese to English Translation button
+                    jp_translation_button.callback = jp_translate_callback
+
+                    # Add the Japanese Translation button to the view
+                    view.add_item(jp_translation_button)
 
                 # Send the original caption message with buttons as a reply to the user
                 original_caption_message = await ctx.send(f"{full_caption.strip()}", files=media_files, view=view, allowed_mentions=discord.AllowedMentions.none())
