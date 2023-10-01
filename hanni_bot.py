b70ba972c1acb9441fa6d24c15f8b526bd051088
jarellgalura <jarellgaluraa@gmail.com>
2023-10-02 00:53:24 +0800

    fix ig

--- hanni_bot.py
+++ hanni_bot.py
@@ -133,7 +133,7 @@ def generate_browser_headers() -> Dict[str, Any]:
     headers = {
         'User-Agent': generate_random_user_agent(),
         'Referer': 'https://www.instagram.com/',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept': '*/*',
         'Accept-Language': 'en-US,en;q=0.5',
         'Upgrade-Insecure-Requests': '1',
         'Cache-Control': 'max-age=0',
