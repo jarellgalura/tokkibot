13e30b1477b602c3c012e70cb82a97f1a7e36393
jarellgalura <83747204+jarellgalura@users.noreply.github.com>
2023-10-01 15:56:29 +0800

    Update hanni_bot.py

--- hanni_bot.py
+++ hanni_bot.py
@@ -48,7 +48,7 @@ conn.commit()
 # Instantiate the TikTok class
 tiktok = TikTok()
 INSTALOADER_SESSION_DIR = os.path.dirname(os.path.abspath(__file__))
-INSTAGRAM_USERNAME = "praychandesu_"
+INSTAGRAM_USERNAME = ""
 
 # Create an Instaloader context with the desired session file name
 L = instaloader.Instaloader(
